88ed62d89d01539a9d072f5c2645730670261be3
edumixer <edu.cena.wf@gmail.com>
2020-09-28 22:44:02 -0300

    Adicionando a validação dos campos do formulario e criando a pagina home

--- projeto/cadastro/index.js
+++ projeto/cadastro/index.js
@@ -2,96 +2,107 @@ var dados = [];
 var banco = window.localStorage;
 
 $(document).ready(function() {
+    fLocalEventosCLick();
     var db = banco.getItem("dadosUsuarios")
     if( db !== null) {
         dados = JSON.parse(db);   
-    }else {
+    } else {
         dados = [];
     }
-    
+function fLocalEventosCLick() {
     $("#salvar-formulario").click(function() {
-        var nome = $("#nome").val();
-        var sobrenome = $("#sobrenome").val();
-        var email = $("#email").val();
-        var senha = $("#senha").val();
-        var confirma_senha = $("#confirma_senha").val();
+
+        fLocalValidaCampos("nome");
+        fLocalValidaCampos("sobrenome");
+        fLocalValidaCampos("email");
+        fLocalValidaCampos("senha");
+        fLocalValidaCampos("confirma_senha");
+
+        fLocalValidaSenha();
         
-        var aux = [];
-        aux.push(nome);
-        aux.push(sobrenome);
-        aux.push(email);
-        aux.push(senha);
-        aux.push(confirma_senha);
+        var aux = {};
+        aux.nome = nome;
+        aux.sobrenome = sobrenome;
+        aux.email = email;
+        aux.senha = senha;
+        aux.confirma_senha = confirma_senha;
         
         dados.push(aux);
         
         banco.setItem("dadosUsuarios", JSON.stringify(dados));
-        //fLocalMostraDados();
-        alert("Seu arquivo foi gravado!");
         
-        
-        if (nome == 0){
-            $("#nome").removeClass("campo-formulario").addClass("campo-errado");
-        }
-        else {
-            $("#nome").removeClass("campo-errado").addClass("campo-formulario");
-        }
-        
-        if (sobrenome == 0){
-            $("#sobrenome").removeClass("campo-formulario").addClass("campo-errado");
-        }
-        else {
-            $("#sobrenome").removeClass("campo-errado").addClass("campo-formulario");
-        }
-        
-        if (email == 0){
-            $("#email").removeClass("campo-formulario").addClass("campo-errado");
-        }
-        else {
-            $("#email").removeClass("campo-errado").addClass("campo-formulario");
-        }
-        
-        if (senha == 0){
-            $("#senha").removeClass("campo-formulario").addClass("campo-errado");
-        }
-        else {
-            $("#senha").removeClass("campo-errado").addClass("campo-formulario");
-        }
-        
-        if (confirma_senha == 0){
-            $("#confirma_senha").removeClass("campo-formulario").addClass("campo-errado");
-        }
-        else {
-            $("#confirma_senha").removeClass("campo-errado").addClass("campo-formulario");
-        }
-        
-        if ((nome != 0) && (sobrenome != 0) && (email != 0) && (senha != 0) && (confirma_senha != 0)) {
-            alert("Voce completou todos os campos");
-        }
-        
-        
-        if (verifica_senha(senha, confirma_senha)) {
-            $("#nome").val("");
-            $("#sobrenome").val("");
-            $("#email").val("");
-            $("#senha").val("");
-            $("#confirma_senha").val("");
-        }
-        
-        // $("salvar-formulario").click(function() {
-        window.location.href = "../home/index.html"
-        // });
+        if (fLocalValidaTodos()) {
+            fLocalLimpaCampos();
+            window.location.href = "../home/index.html"
+        } 
+            
     });
+}
     
-    function verifica_senha(senha, confirma_senha) {
-        if (senha !== confirma_senha) {
-            alert("Por favor, insira uma senha igual");
-            $("#senha").removeClass("campo-formulario").addClass("campo-errado");
-            $("#confirma_senha").removeClass("campo-formulario").addClass("campo-errado");
+    function fLocalValidaCampos(idCampo) {
+        var valor = $("#" + idCampo).val();
+
+        if(valor == "") {
+            $("#" + idCampo).addClass("campo-errado");
+            return false;
+        } else {
+            $("#" + idCampo).removeClass("campo-errado");
+            return true;
+        }
+    }
+
+    function fLocalValidaTodos() {
+        
+        
+        if(fLocalValidaCampos("nome") === false) {
+            return false;
+        } 
+        if (fLocalValidaCampos("sobrenome") === false) {
             return false;
         }
-        return true;
+        if (fLocalValidaCampos("email") === false) {
+            return false;
+        }
+        if (fLocalValidaCampos("senha") === false) {
+            return false;
+        }
+        if (fLocalValidaCampos("confirma_senha") === false) {
+            return false;
+        }
+        if (fLocalValidaSenha() === false) {
+            return false;
+        }
+        else {
+            return true;
+        }
+        
+    }   
+
+    function fLocalValidaSenha() {
+        var senha = $("#senha").val();
+        var confirma_senha = $("#confirma_senha").val();
+        
+        if(senha != "") {
+            if(senha != confirma_senha) {
+                $("#confirma_senha").addClass("campo-errado");
+                return false;
+            }
+            else {
+                $("#confirma_senha").removeClass("campo-errado");
+                return true;
+            }
+        }
     }
+
+    function fLocalLimpaCampos() {
+        $("#nome").val("");
+        $("#sobrenome").val("");
+        $("#email").val("");
+        $("#senha").val("");
+        $("#confirma_senha").val("");
+    }
+    
+
     
 });
 
